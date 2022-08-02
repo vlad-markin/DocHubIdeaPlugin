fcc22c90961e7e17839f8a7b6d93c8aa70e1b3b8
R.Piontik <r.piontik@mail.ru>
2022-08-02 10:41:23 +0300

    Возникает проблемы вызова диалогового окна сохранения картинки. Пытаюсь решить через отложенный запуск таймером.

--- src/main/java/org/dochub/idea/arch/tools/Download.java
+++ src/main/java/org/dochub/idea/arch/tools/Download.java
@@ -10,27 +10,36 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Download {
     static public void download(String content, String title, String description) {
-        ApplicationManager.getApplication().invokeLater(new Runnable() {
-            @Override
+        Timer timer = new Timer("Download diagram");
+        timer.schedule(new TimerTask() {
             public void run() {
-                FileSaverDescriptor descriptor = new FileSaverDescriptor(title, description, new String[]{"svg"});
-                FileSaverDialog dialog = FileChooserFactory.getInstance().createSaveFileDialog(descriptor, (Project) null);
-                VirtualFileWrapper vf = dialog.save((VirtualFile) null, "diagram");
+                ApplicationManager.getApplication().invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        FileSaverDescriptor descriptor = new FileSaverDescriptor(title, description, new String[]{"svg"});
+                        FileSaverDialog dialog = FileChooserFactory.getInstance().createSaveFileDialog(descriptor, (Project) null);
+                        VirtualFileWrapper vf = dialog.save((VirtualFile) null, "diagram");
 
-                if (vf == null) {
-                    return;
-                }
-                File file = vf.getFile();
-                try {
-                    BufferedWriter writer = new BufferedWriter(new FileWriter(file, false));
-                    writer.append(content);
-                    writer.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                        if (vf == null) {
+                            return;
+                        }
+                        File file = vf.getFile();
+                        try {
+                            BufferedWriter writer = new BufferedWriter(new FileWriter(file, false));
+                            writer.append(content);
+                            writer.close();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
             }
-        });
+        }, 100L);
+
     }
 }
