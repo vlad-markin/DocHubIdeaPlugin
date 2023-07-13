6cdaf3594856caa7946f6758763cd4bca7b1b546
Temnyakov Nikolay <temnjakovn@gmail.com>
2023-07-13 22:22:33 +0300

    Исправлена ошибка валидации jsonata

--- src/main/kotlin/org/dochub/idea/arch/annotator/JSONataAnnotator.kt
+++ src/main/kotlin/org/dochub/idea/arch/annotator/JSONataAnnotator.kt
@@ -5,23 +5,21 @@ import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.psi.PsiElement
-import org.intellij.sdk.language.psi.JSONataCall
-import org.intellij.sdk.language.psi.JSONataDeep
-import org.intellij.sdk.language.psi.JSONataSysFunction
+
 
 class JSONataAnnotator: Annotator {
 
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
 
-        val isJsonataFunction = element is JSONataSysFunction
-        val isCallFunction = element is JSONataCall
+//        val isJsonataFunction = element is JSONataSysFunction
+//        val isCallFunction = element is JSONataCall
         val firstChild = element.firstChild
 
-        if (isJsonataFunction || isCallFunction) {
-            holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
-                .range(firstChild)
-                .textAttributes(DefaultLanguageHighlighterColors.FUNCTION_DECLARATION)
-                .create()
-        }
+//        if (isJsonataFunction || isCallFunction) {
+//            holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+//                .range(firstChild)
+//                .textAttributes(DefaultLanguageHighlighterColors.FUNCTION_DECLARATION)
+//                .create()
+//        }
     }
 }
--- src/main/kotlin/org/dochub/idea/arch/completion/JSONataPatterns.kt
+++ src/main/kotlin/org/dochub/idea/arch/completion/JSONataPatterns.kt
@@ -3,8 +3,6 @@ package org.dochub.idea.arch.completion
 import com.intellij.patterns.PlatformPatterns.psiElement
 import com.intellij.patterns.PsiElementPattern
 import com.intellij.psi.PsiElement
-import org.intellij.sdk.language.psi.JSONataArguments
-import org.intellij.sdk.language.psi.JSONataDeclFunction
 
 
 object JSONataPatterns {
@@ -12,27 +10,7 @@ object JSONataPatterns {
     const val ASSIGN = ":="
 
     object ExpressionPatterns {
-        fun inPattern(): PsiElementPattern.Capture<PsiElement> {
-            return psiElement().andOr(
-                psiElement().atStartOf(psiElement(JSONataArguments::class.java)))
-        }
 
-        fun orPattern(): PsiElementPattern.Capture<PsiElement> {
-            return psiElement().andOr(
-                psiElement().atStartOf(psiElement(JSONataArguments::class.java)))
-        }
-
-        fun functionPattern(): PsiElementPattern.Capture<PsiElement> {
-            return psiElement().andOr(
-                psiElement().atStartOf(psiElement(JSONataDeclFunction::class.java)),
-                psiElement().afterLeaf(ASSIGN))
-        }
-
-
-        fun andPattern(): PsiElementPattern.Capture<PsiElement> {
-            return psiElement().andOr(
-                psiElement().atStartOf(psiElement(JSONataArguments::class.java)))
-        }
     }
 
     object DeclPatterns {
